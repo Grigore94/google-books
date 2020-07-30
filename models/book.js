fcd534b916dc8b9ec6f0c608ab94bc01ad3b7d66
Grigore <grigore.mereacre95@gmail.com>
2020-07-30 14:48:40 -0500

    added schema

---
+++ models/book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bSchema = new Schema({
+  title: { type: String, required: true },
+  authors: { type: String, required: true },
+  synophis: String,
+  image: { type: String, trim: true },
+  link: { type: String, trim: true },
+  data: { type: Data, default: DataTransfer.now },
+});
+const Book = mongoose.model("Book", bSchema);
+
+module.exports = Book;
